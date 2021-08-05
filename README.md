455cbb9343f421a8e5f7370ed319a6aa1fae3df2
marmooo <marmooo@users.noreply.github.com>
2021-08-06 07:00:32 +0900

    deno lint

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # 三字熟語さがし
+
 [三字熟語を探しながら楽しく勉強できるゲーム](https://marmooo.github.io/jukugo3-sagashi/)です。
 
 ## Dataset
-- kanji-3-5000 in [marmooo/ngram-idioms](https://github.com/marmooo/ngram-idioms)
+
+- kanji-3-5000 in
+  [marmooo/ngram-idioms](https://github.com/marmooo/ngram-idioms)
+
+## Build
+
+`bash build.sh`
 
 ## License
+
 MIT
--- src/sw.js
+++ src/sw.js
@@ -1,45 +1,44 @@
-var CACHE_NAME = '2021-06-24 07:06';
+var CACHE_NAME = "2021-08-06 07:00";
 var urlsToCache = [
-  '/jukugo3-sagashi/',
-  '/jukugo3-sagashi/index.js',
-  '/jukugo3-sagashi/favicon/original.svg',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+  "/jukugo3-sagashi/",
+  "/jukugo3-sagashi/index.js",
+  "/jukugo3-sagashi/favicon/original.svg",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      }),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      }),
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", function (event) {
   var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
