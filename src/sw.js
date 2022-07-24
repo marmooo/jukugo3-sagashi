0afca8e56e9dd91419e70be4eabcc20f4ceb80c6
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:07 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
-var CACHE_NAME = "2022-06-30 08:20";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/jukugo3-sagashi/",
   "/jukugo3-sagashi/index.js",
   "/jukugo3-sagashi/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
