2dc08be02f6df4405720326d974599896dacb62c
marmooo <marmooo@users.noreply.github.com>
2024-09-19 11:21:19 +0900

    use TextLineStream

--- build.js
+++ build.js
@@ -1,13 +1,14 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 async function build(threshold) {
   const result = [];
   for (let i = 1; i <= 12; i++) {
     const idioms = [];
-    const fileReader = await Deno.open(
-      `graded-idioms-ja/dist/${i}.csv`,
-    );
-    for await (const line of readLines(fileReader)) {
+    const file = await Deno.open(`graded-idioms-ja/dist/${i}.csv`);
+    const lineStream = file.readable
+      .pipeThrough(new TextDecoderStream())
+      .pipeThrough(new TextLineStream());
+    for await (const line of lineStream) {
       const arr = line.split(",");
       const word = arr[0];
       const count = parseInt(arr[1]);
